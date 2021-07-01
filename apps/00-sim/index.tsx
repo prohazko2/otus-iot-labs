0baed17f1e16686e6dc1fa075b26de2dba3c5a02
Oleg Prohazko <olegprohazko@gmail.com>
2021-07-01 23:15:52 +0300

    00-sim move avr runner to own folder

--- apps/00-sim/index.tsx
+++ apps/00-sim/index.tsx
@@ -3,7 +3,7 @@ import "@wokwi/elements";
 import React from "react";
 import { render } from "react-dom";
 
-import { AVRRunner } from "./avr";
+import { AVRRunner } from "./avr/runner";
 import { PinState } from "avr8js";
 
 import hex from "./test.ino.hex";
@@ -17,23 +17,22 @@ avr.execute();
 
 class App extends React.Component {
   state = {
-    led: false,
+    high: false,
   };
 
   componentDidMount() {
     avr.portB.addListener(() => {
-      this.setState({
-        led: avr.portB.pinState(13 - 8) === PinState.High,
-      });
+      const high = avr.portB.pinState(13 - 8) === PinState.High;
+      this.setState({ high });
     });
   }
 
   render() {
-    const { led } = this.state;
+    const { high } = this.state;
     return (
       <div>
-        <wokwi-arduino-uno led13={led || undefined}></wokwi-arduino-uno>
-        <wokwi-led value={led || undefined} color="red" label="13"></wokwi-led>
+        <wokwi-arduino-uno led13={high || undefined}></wokwi-arduino-uno>
+        <wokwi-led value={high || undefined} color="red" label="13"></wokwi-led>
       </div>
     );
   }
