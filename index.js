f9f45d8e6fc654da5ea239c00c02d59a8abb1104
Oleg Prohazko <prohazko@rightech.io>
2021-07-27 19:21:18 +0300

    move docs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const db = require("./db");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
